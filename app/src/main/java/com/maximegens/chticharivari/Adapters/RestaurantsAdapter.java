7f829d488e6fbe07ef964633ca8b75afb286c2b1
maximegens <maximegens@gmail.com>
2015-05-13 16:38:33 +0200

    Ajout de restaurants manquants + dossier Asset avec description des restaurants

--- app/src/main/java/com/maximegens/chticharivari/Adapters/RestaurantsAdapter.java
+++ app/src/main/java/com/maximegens/chticharivari/Adapters/RestaurantsAdapter.java
@@ -14,40 +14,64 @@ import com.maximegens.chticharivari.R;
 import java.util.ArrayList;
 
 /**
- * Created by Maxime on 13/05/2015.
+ * Adapter pour la liste des restaurants ChTiCharivari.
  */
 public class RestaurantsAdapter extends BaseAdapter {
 
-    // Une liste de restaurants
+    // Une liste de restaurants chti Charivari.
     private ArrayList<RestaurantChti> listRestaurants;
 
-    //Le contexte dans lequel est present notre adapter
+    //Le contexte dans lequel est present notre adapter.
     private Context mContext;
 
-    //Un mecanisme pour gerer l'affichage graphique depuis un layout XML
+    //Un mecanisme pour gerer l'affichage graphique depuis un layout XML.
     private LayoutInflater mInflater;
 
+    /**
+     *     Constructeur de l'adapter.
+     */
     public RestaurantsAdapter(Context context, ArrayList<RestaurantChti> listeRestaurants) {
         this.mContext = context;
         this.listRestaurants = listeRestaurants;
         this.mInflater = LayoutInflater.from(mContext);
     }
 
+    /**
+     * Permet de connaitre le nombre d'item ( de restaurants) dans la liste.
+     * @return Le nombre (int) de restaurants dans la liste.
+     */
     @Override
     public int getCount() {
         return this.listRestaurants.size();
     }
 
+    /**
+     * Permet de retourner le restaurant à la position passé en paramétre.
+     * @param position La position de l'objet demandé.
+     * @return Un ChtiCharivari correspond à la position demandée.
+     */
     @Override
     public Object getItem(int position) {
         return this.listRestaurants.get(position);
     }
 
+    /**
+     * Permet de retourner l'id du restaurant à la position passé en paramétre.
+     * @param position La position de l'objet demandé.
+     * @return L'id du ChtiCharivari correspond à la position demandée.
+     */
     @Override
     public long getItemId(int position) {
         return position;
     }
 
+    /**
+     * Défini et construis la vue pour chaque items.
+     * @param position la position de l'item dans la liste.
+     * @param convertView La vue a convertir.
+     * @param parent Le parents.
+     * @return La vue associée avec les valeurs.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LinearLayout layoutItem;
--- app/src/main/java/com/maximegens/chticharivari/Beans/LesRestaurantsChti.java
+++ app/src/main/java/com/maximegens/chticharivari/Beans/LesRestaurantsChti.java
@@ -5,36 +5,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Maxime on 13/05/2015.
- */
 public class LesRestaurantsChti {
     /**
      * An array of sample (dummy) items.
      */
-    public static ArrayList<RestaurantChti> lesRestaurants = new ArrayList<RestaurantChti>();
+    public static ArrayList<RestaurantChti> lesRestaurants = new ArrayList<>();
 
     static{
-        RestaurantChti calais = new RestaurantChti("1","Calais");
-        RestaurantChti wimereux = new RestaurantChti("2","Wimereux ");
-        RestaurantChti boulogneSurMer = new RestaurantChti("3","Boulogne sur Mer");
-        RestaurantChti leTouquet = new RestaurantChti("4","Le Touquet");
-        RestaurantChti aireSurLaLys = new RestaurantChti("5","Air sur la Lys");
-        RestaurantChti bethune = new RestaurantChti("6","Bethune");
+        RestaurantChti aireSurLaLys = new RestaurantChti("1","Air sur la Lys");
+        RestaurantChti amiens = new RestaurantChti("2","Amiens");
+        RestaurantChti arras = new RestaurantChti("3","Arras");
+        RestaurantChti bethune = new RestaurantChti("4","Bethune");
+        RestaurantChti boulogneSurMer = new RestaurantChti("5","Boulogne sur Mer");
+        RestaurantChti calais = new RestaurantChti("6","Calais");
         RestaurantChti douai = new RestaurantChti("7","Douai");
-        RestaurantChti valenciennes = new RestaurantChti("8","Valenciennes");
-        RestaurantChti arras = new RestaurantChti("9","Arras");
-        RestaurantChti amiens = new RestaurantChti("10","Amiens");
-        lesRestaurants.add(calais);
-        lesRestaurants.add(wimereux);
-        lesRestaurants.add(boulogneSurMer);
-        lesRestaurants.add(leTouquet);
+        RestaurantChti dunkerque = new RestaurantChti("8","Dunkerque");
+        RestaurantChti heninbeaumont = new RestaurantChti("9","Henin-Beaumont");
+        RestaurantChti leTouquet = new RestaurantChti("10","Le Touquet");
+        RestaurantChti lomme = new RestaurantChti("11","Lomme");
+        RestaurantChti saintOmer = new RestaurantChti("12","Saint-Omer");
+        RestaurantChti valenciennes = new RestaurantChti("13","Valenciennes");
+        RestaurantChti villeneuveAscq = new RestaurantChti("14","Villeneuve d'ascq");
+        RestaurantChti wimereux = new RestaurantChti("15","Wimereux ");
+
         lesRestaurants.add(aireSurLaLys);
-        lesRestaurants.add(bethune);
-        lesRestaurants.add(douai);
-        lesRestaurants.add(valenciennes);
-        lesRestaurants.add(arras);
         lesRestaurants.add(amiens);
+        lesRestaurants.add(arras);
+        lesRestaurants.add(bethune);
+        lesRestaurants.add(boulogneSurMer);
+        lesRestaurants.add(calais);
+        lesRestaurants.add(douai);
+        lesRestaurants.add(dunkerque);
+        lesRestaurants.add(heninbeaumont);
+        lesRestaurants.add(leTouquet);
+        lesRestaurants.add(lomme);
+        lesRestaurants.add(saintOmer);
+        lesRestaurants.add(valenciennes);
+        lesRestaurants.add(villeneuveAscq);
+        lesRestaurants.add(wimereux);
+
     }
 
     public ArrayList<RestaurantChti> getRestaurants(){
